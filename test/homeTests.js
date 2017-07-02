0ede472949e96b0112289f4eb55ab46dc0cd1927
Arnaud Flaesch <arnaudflaesch@hotmail.com>
2017-07-02 16:38:19 +0200

    Update Travis CI env and drop user database for tests

--- test/homeTests.js
+++ test/homeTests.js
@@ -1,7 +1,8 @@
 const server = require("../src/server"),
     assert = require('assert'),
     request = require("supertest"),
-    should = require('should');
+    should = require('should'),
+    User = require("../src/model/user");
 
 describe("server", function () {
     before(function () {
@@ -15,6 +16,10 @@ describe("server", function () {
 
 describe("Server status and Message", function () {
     it("status response should be equal 200", function (done) {
+        User.remove({}, function(err) {
+            console.log('collection removed')
+        });
+
         request(server)
             .get('/')
             .expect(200)
